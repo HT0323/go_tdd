537354d32acf5c9a6e2d992f74f09900aa262ef4
HaraT0323 <alra9si916323@gmail.com>
2022-11-03 20:03:27 +0900

    Hello関数に名前を受け取るテストを追加

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Masa")
+	want := "Hello, Masa"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)

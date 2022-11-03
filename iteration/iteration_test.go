0859aa6d194c3fee13007438afb2328423fddca3
HaraT0323 <alra9si916323@gmail.com>
2022-11-03 22:58:45 +0900

    Repeat関数に繰り返し数を渡すテストを追加

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)

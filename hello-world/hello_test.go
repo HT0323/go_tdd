59345b404c6a6fa955018d041deea1f79d975fdf
HaraT0323 <alra9si916323@gmail.com>
2022-11-03 22:08:18 +0900

    Hello関数の引数に空文字が来たパターンのテストを追加

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Masa")
-	want := "Hello, Masa"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Masa")
+		want := "Hello, Masa"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello, World", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }

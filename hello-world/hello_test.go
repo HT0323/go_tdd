441f7a01f5c5aa2c4e0bd4e02a90dbd19f0d3d59
HaraT0323 <alra9si916323@gmail.com>
2022-11-03 22:21:31 +0900

    Hello関数がスペイン語で出力される処理を追加

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -11,15 +11,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Masa")
+		got := Hello("Masa", "")
 		want := "Hello, Masa"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying hello, World", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Aki", "Spanish")
+		want := "Hola, Aki"
 		assertCorrectMessage(t, got, want)
 	})
 

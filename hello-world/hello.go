0bc8f03594a7bea997b8f8e390df4d4c38c2baef
HaraT0323 <alra9si916323@gmail.com>
2022-11-03 22:21:09 +0900

    Hello関数がスペイン語で出力されるテストを追加

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Masa"))
+	fmt.Println(Hello("Masa", "Spanish"))
 }

3aa85dc15bf5db8ec1b1f958f2c10cb58b6b2662
HaraT0323 <alra9si916323@gmail.com>
2022-11-04 19:34:38 +0900

    Withdrawメソッドで残高以上を引き出そうとした場合のテストを追加

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -24,4 +24,12 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+	startingBalance := Bitcoin(20)
+	wallet := Wallet{startingBalance}
+	err := wallet.Withdraw(Bitcoin(100))
+	assertBalance(t, wallet, startingBalance)
+
+	if err == nil {
+		t.Errorf("wanted an error but didn't get one")
+	}
 }

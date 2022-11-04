ab8492c9891ac453ecb6a771f67f86656c276480
HaraT0323 <alra9si916323@gmail.com>
2022-11-04 18:10:12 +0900

    Area関数にtriangleの場合の処理を追加

--- structs/shapes.go
+++ structs/shapes.go
@@ -19,6 +19,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Redius * c.Redius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }

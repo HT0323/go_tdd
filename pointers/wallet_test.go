e7d6a878f98d0b72c5a5959106c61e1ae433843b
HaraT0323 <alra9si916323@gmail.com>
2022-11-04 19:28:04 +0900

    Withdraw, Depositメソッドのテストをリファクタリング

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,25 +5,23 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-		got := wallet.Balance()
-		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("Withdraw", func(t *testing.T) {
-		Wallet := Wallet{balance: Bitcoin(20)}
-		Wallet.Withdraw(Bitcoin(10))
-		got := Wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }

f88b7df2415844f8c743cb8f9efec0f31924e831
HaraT0323 <alra9si916323@gmail.com>
2022-11-06 00:48:49 +0900

    ConvertToRoman関数に9 ~ 39を与えた場合の処理を追加

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -2,20 +2,26 @@ package main
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 

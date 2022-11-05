08202df170a8dc1be9921877e40251bd2fa292e9
HaraT0323 <alra9si916323@gmail.com>
2022-11-06 00:21:18 +0900

    ConvertToRoman関数に4を与えた場合の処理を追加

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -4,7 +4,11 @@ import "strings"
 
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 

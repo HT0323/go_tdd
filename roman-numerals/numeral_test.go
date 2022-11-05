83e38932aad7da6cce52b1b789216c64120cb7a1
HaraT0323 <alra9si916323@gmail.com>
2022-11-05 23:59:36 +0900

    ConvertToRoman関数のテストを追加

---
+++ roman-numerals/numeral_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	got := ConvertToRoman(1)
+	want := "I"
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}

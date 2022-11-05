f5dc3cb343c6f53b75dea51192908176f6db1c01
HaraT0323 <alra9si916323@gmail.com>
2022-11-05 19:42:36 +0900

    Racer関数の処理を並列処理をできるように修正

--- select/racer.go
+++ select/racer.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }

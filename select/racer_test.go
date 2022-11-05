2afe969f49eb60a31798b4b7f8a1541497718c89
HaraT0323 <alra9si916323@gmail.com>
2022-11-05 18:55:01 +0900

    Racer関数のテストを追加

---
+++ select/racer_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestRacer(t *testing.T) {
+	slowURL := "http://www/facebook.com"
+	fastURL := "http://wwww.quii.co.uk"
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}

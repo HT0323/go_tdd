a837b760109e83b240999858e27038f32777a1be
HaraT0323 <alra9si916323@gmail.com>
2022-11-05 23:03:53 +0900

    テスト内のCounter構造体をポインター型に変更

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
@@ -16,7 +16,7 @@ func TestCounter(t *testing.T) {
 	})
 	t.Run("it runs safely concurrently", func(t *testing.T) {
 		wantedCount := 1000
-		counter := Counter{}
+		counter := NewCounter()
 
 		var wg sync.WaitGroup
 		wg.Add(wantedCount)
@@ -32,9 +32,13 @@ func TestCounter(t *testing.T) {
 	})
 
 }
-func assertCounter(t *testing.T, got Counter, want int) {
+func assertCounter(t *testing.T, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
 	}
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
 }

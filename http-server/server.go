7e0f90c81787ad27732902fcd599ba1851f133e4
HaraT0323 <alra9si916323@gmail.com>
2022-11-06 12:44:37 +0900

    PlayerServer関数に他ユーザーを渡した場合の処理を追加

--- http-server/server.go
+++ http-server/server.go
@@ -3,8 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, 20)
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
 }

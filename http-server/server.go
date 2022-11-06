f7b798637dadd525061608a90d388e078b4e4918
HaraT0323 <alra9si916323@gmail.com>
2022-11-07 00:19:05 +0900

    存在しないユーザをパスパラメータに指定した場合の処理を追加

--- http-server/server.go
+++ http-server/server.go
@@ -16,8 +16,12 @@ type PlayerServer struct {
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+	score := p.store.GetPlayerScore(player)
 
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(w, score)
 }
 
 func GetPlayerScore(name string) string {

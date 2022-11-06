294659cce239d450e15f81fd263e4f06fc1e5c3f
HaraT0323 <alra9si916323@gmail.com>
2022-11-06 12:44:24 +0900

    PlayerServer関数に他ユーザーを渡した場合のテストを追加

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -18,4 +18,17 @@ func TestGETPlayers(t *testing.T) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }

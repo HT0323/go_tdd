d22792800ddcad04c2ad82aef06559d3eed4967e
HaraT0323 <alra9si916323@gmail.com>
2022-11-06 12:56:40 +0900

    PlayerServer関数の処理をリファクタリング

--- http-server/server.go
+++ http-server/server.go
@@ -8,12 +8,15 @@ import (
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	if player == "Pepper" {
-		fmt.Fprint(w, "20")
-		return
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
 	}
-	if player == "Floyd" {
-		fmt.Fprint(w, "10")
-		return
+	if name == "Floyd" {
+		return "10"
 	}
+	return ""
 }

de9f8e469f813fa1e452f96f5bca0946501e44bc
HaraT0323 <alra9si916323@gmail.com>
2022-11-08 00:24:51 +0900

    スコア保存リクエストのテストを追加

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -50,6 +50,23 @@ func TestGETPlayers(t *testing.T) {
 	})
 }
 
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+
+}
+
 func assertStatus(t *testing.T, got, want int) {
 	t.Helper()
 	if got != want {
